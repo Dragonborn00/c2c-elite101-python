1ed2ca03bf8977e82da1276cf8ead4ef7b0d3b8d
Dragonborn00 <82978127+Dragonborn00@users.noreply.github.com>
2025-01-29 01:32:05 +0000

    Main.py

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-
-"""
-    Welcome to Elite 101 this program is a starter for your chatbot project.
-    The starter prompts the user to enter their name and then greets them with a personalized message.
-
-    Functions:
-        get_user_name(): Prompts the user to enter their name and returns it.
-        greet_user(name): Prints a greeting message using the provided name.
-        main(): Main function that orchestrates the user input and greeting process.
-
-    Execution:
-        When the script is run directly (not imported as a module), it will execute the main() function.
-"""
-
-
 def get_user_name():
     return input("Please enter your name: ")
 
 def greet_user(name):
     print(f"Hello, {name}!")
 
-def main():
+def get_user_age(name):
+    return input(f"Okay {name}, now I will need your age since some of the items on the menu might be age restricted ")
+
+def main ():
     user_name = get_user_name()
     greet_user(user_name)
+    user_age = get_user_age(user_name)
+
+    print ("\nWhat can i get for you today? Please choose an option below")
+    print ("1. Order food")
+    print ("2. Track Your Delivery")
+    print ("3. Speak to support")
+    print ("4. Exit")
+
+    while True:
+        choice = input("Enter the number of your choice: ")
+
+
+        if choice == "1":
+            print ("Great! What would you like to order?")
+            break
+        elif choice == "2":
+            print ("Tracking your delivery...")
+            break
+        elif choice == "3":
+            print ("connecting you to support")
+            break
+        elif choice == "4":
+            print ("Thanks for stopping by hope to see you again soon!")
+            break
+        else:
+            print("Uh Oh! that doesn't seem to be a valid option. Please try again")
 
 if __name__ == "__main__":
+    print("Hi, and welcome to Bitebot. I'm here to get rid of those cravings, so tell me what you want and, you can leave the rest to me!")
     main()
